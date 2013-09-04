4904cf477521f43e38a0a4d7cb8d2c8d4bceeab8
Tim Roediger <superdweebie@gmail.com>
2013-09-05 08:42:04 +1000

    add some more performance tests

--- tests/performance/index.php
+++ tests/performance/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$loaderPath = 'vendor/autoload.php';
+$loaderPath = 'vendor/zoopcommerce/slipstream/autoload.php';
 
 // Root if testing independently
 $applicationRoot = __DIR__ . '/../../';
@@ -14,6 +14,16 @@ chdir($applicationRoot);
 
 $loader = require_once($loaderPath);
 $loader->add('Zoop\\ShardModule\\Test', __DIR__ . '/../');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    //use juggernaut cache for GET requests
+    $pageTtl = 1; //1 seconds, very low cache time, but helps page speed massively with concurrent requests
+    $cacheDirectory = __DIR__ . '/cache/fullpage';
+
+    $adapter = new Zoop\Juggernaut\Adapter\FileSystem($cacheDirectory);
+    $pageCache = new Zoop\Juggernaut\Helper\FullPage($adapter, $pageTtl, true, false);
+    $pageCache->start();
+}
 
 // Run the application!
 Zend\Mvc\Application::init(require __DIR__ . '/performance.application.config.php')->run();
---
+++ tests/performance/test-nested-embed-get.sh
@@ -0,0 +1,1 @@
+/c/xds/Apache24/bin/ab -n 1000 -c 50 -H "Accept:application/json" http://localhost/performance/rest/game/feed-the-kitty/publisher
